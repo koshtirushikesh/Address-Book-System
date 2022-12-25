60b40d0a1466b045991572979953f0c3e226dfd2
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-25 09:31:38 +0530

    Create a Contacts

---
+++ ContactsArrayList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class ContactsArrayList
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip;
+        public int Phone_Number;
+        public string E_mail_Id;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book System");
+            ContactsArrayList contactsArrayList = new ContactsArrayList();
+            Console.WriteLine("Enter the first name :");
+            contactsArrayList.First_Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name :");
+            contactsArrayList.Last_Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address :");
+            contactsArrayList.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the city name :");
+            contactsArrayList.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the state name :");
+            contactsArrayList.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the zip number :");
+            contactsArrayList.Zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the phone number :");
+            contactsArrayList.Phone_Number = (int)Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter the E mail id :");
+            contactsArrayList.E_mail_Id = Console.ReadLine();
+
+            Console.WriteLine(contactsArrayList.First_Name);
+            Console.WriteLine(contactsArrayList.Last_Name);
+            Console.WriteLine(contactsArrayList.Address);
+            Console.WriteLine(contactsArrayList.City);
+            Console.WriteLine(contactsArrayList.State);
+            Console.WriteLine(contactsArrayList.Zip);
+            Console.WriteLine(contactsArrayList.Phone_Number);
+            Console.WriteLine(contactsArrayList.E_mail_Id);
+            
+        }
+    }
+}
